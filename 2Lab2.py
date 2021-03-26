e4b24090235921ae6e4e0373c89c1d36979ffeee
Ольга Серова <moroz9704@icloud.com>
2021-03-26 14:21:17 +0300

    исправила

--- 2Lab2.py
+++ 2Lab2.py
@@ -1,10 +1,9 @@
 #3 вариант 2задача
 list_phone_number = ['+7912123456', '+7915213456', '+6915213456', '+4915213456', '+7915213456']
-list_index = []
+list_phone_number1 = []
 for number_phone in list_phone_number:
-    if number_phone.count('+7'):
-        list_index.append(number_phone)
-for index in list_index:
-        list_phone_number.remove(index)
+    start_number = number_phone[0] + number_phone[1]
+    if start_number != '+7':
+        list_phone_number1.append(number_phone)
 
-print(list_phone_number)
+print(list_phone_number1)
