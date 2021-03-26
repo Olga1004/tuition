87ddae717b3852aca0b41512a4ab8389f942613b
Ольга Серова <moroz9704@icloud.com>
2021-03-26 14:59:02 +0300

    испр.clock

--- clock.py
+++ clock.py
@@ -4,9 +4,9 @@ class Clock:
         self.minutes = minutes
         self.seconds = seconds
     def __add__(self, other):
-        return {'hours': self.hours + other.hours, 'minutes': self.minutes + other.minutes, 'seconds': self.seconds + other.seconds}
+        return Clock(self.hours + other.hours,  self.minutes + other.minutes, self.seconds + other.seconds)
     def __sub__(self, other):
-        return {'hours': self.hours - other.hours, 'minutes': self.minutes - other.minutes, 'seconds': self.seconds - other.seconds}
+        return Clock(self.hours - other.hours, self.minutes - other.minutes, self.seconds - other.seconds)
     def __convert__(hours, minutes, seconds):
         return hours * 60 * 60 + minutes * 60 + seconds
     def __eq__(self, other):
@@ -19,19 +19,18 @@ class Clock:
         return time1 != time2
 t1 = Clock(16, 20, 30)
 t2 = Clock(9, 15, 26)
-t3 = Clock.__add__(t1, t2)
-t4 = Clock.__sub__(t1, t2)
-t5 = Clock.__eq__(t1, t2)
-t6 = Clock.__ne__(t1, t2)
-print("Результат сложения: " + str(t3['hours']) + ":" + str(t3['minutes']) + ":" + str(t3['seconds']))
-print("Результат вычетания: " + str(t4['hours']) + ":" +  str(t4['minutes']) + ":" + str(t4['seconds']))
-if t5 == True:
+t3 = t1 + t2
+print(t3)
+t4 = t3 + t2
+print(t4)
+t5 = t1 - t2
+print(t5)
+t6 = Clock.__eq__(t1, t2)
+t7 = Clock.__ne__(t1, t2)
+print("Результат сложения: " + str(t3.hours) + ":" + str(t3.minutes) + ":" + str(t3.seconds))
+print("Результат сложения: " + str(t4.hours) + ":" + str(t4.minutes) + ":" + str(t4.seconds))
+print("Результат вычетания: " + str(t5.hours) + ":" +  str(t5.minutes) + ":" + str(t5.seconds))
+if t6 == True:
     print("Равно")
-else:
+if t7 != False:
     print("Не равно")
-
-
-
-
-
-
