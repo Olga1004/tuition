d7eeb421efcd57674a2803f7ba500f6d85dc8c6d
Ольга Серова <moroz9704@icloud.com>
2021-03-28 20:10:17 +0300

    испр. 1лаб, 6 вар, 1 задача

--- finishtask.py
+++ finishtask.py
@@ -1,6 +1,7 @@
-#1 лаб.6 вар, 1 задача
+# 1 лаб.6 вар, 1 задача
+def my_function():
+    for i in range(-20, 50, 1):
+        x = i/10
+        print(x**2 + 5)
 
-for i in range(-2, 5):
-    x = i / 10
-print(x, x**2 + 5)
-
+my_function()
