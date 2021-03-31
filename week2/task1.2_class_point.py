8daa8cc05a43e33fd182a13edff07ab13850656a
Ольга Серова <moroz9704@icloud.com>
2021-03-31 18:12:36 +0300

    1 и 2 задача class point

---
+++ week2/task1.2_class_point.py
@@ -0,0 +1,52 @@
+# 1
+class Point3D:
+    x = [1]
+    y = [2]
+
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def get(self):
+        return self.x, self.y
+
+    def convert(self):
+        return self.x, self.y
+
+
+pt1 = Point3D(2, 3)
+print(pt1.__dict__)
+print(pt1.convert())
+
+
+# 2
+class Point:
+
+    def __init__(self, point=None):
+        if point is None:
+            self.x = 0
+            self.y = 0
+        else:
+            self.x = point.x
+            self.y = point.y
+
+
+p2 = Point()
+p2.x = 16
+p2.y = 12
+p3 = Point(p2)
+
+print(p3.__dict__)
+
+
+
+
+
+
+
+
+
+
+
+
+
