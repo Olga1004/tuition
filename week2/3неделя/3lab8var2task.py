a17c9e80654f67aceead64f3a68dd2382484ee84
Ольга Серова <moroz9704@icloud.com>
2021-04-07 16:14:15 +0300

    fixes

--- week2/3неделя/3lab8var2task.py
+++ week2/3неделя/3lab8var2task.py
@@ -1,12 +1,11 @@
-# 3 лаб.8 вариант 2 задача
 from random import random
 
 n = int(input("Введите n = "))
 def x(n):
-    x = 1
-    for i in range(n):
-        yield x
-        x = 0.9 * x + 0.43 * random()
+    if n <= 0:
+        return 0
+    else:
+        return 0.9 * x(n-1) + 0.43 * random()
 
-
-print(list(x(n)))
+x(n)
+print(x(n))
