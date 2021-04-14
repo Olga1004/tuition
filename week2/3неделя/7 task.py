e225bcdfab8a6193275f836a6dad4ff506c8568f
Ольга Серова <moroz9704@icloud.com>
2021-04-14 13:17:29 +0300

    7 task corrected

--- week2/3неделя/7 task.py
+++ week2/3неделя/7 task.py
@@ -1,6 +1,5 @@
 # pавномерное распределение тем (цикл фор)
 
-from random import randint, choice
 list_themes = ['python', 'js', 'html', 'css', 'SQl', 'Oracle database', 'php', 'Ruby', 'swift', 'go', 'c++']
 list_date = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 data = {}
@@ -8,10 +7,12 @@ themes_per_day = len(list_themes) // len(list_date)
 remaining_themes = len(list_themes) % len(list_date)
 
 for i in range(len(list_date)):
-    data[list_date[i]] = [choice(list_themes) for i in range(themes_per_day)]
+    new_themes = i * themes_per_day
+    data[list_date[i]] = list_themes[new_themes: new_themes + themes_per_day]
     if remaining_themes > 0:
-        data[list_date[i]].append(list_themes[randint(0, len(list_themes))])
+        data[list_date[i]].append(list_themes[-1])
         remaining_themes -= 1
+
 
 for i in data.items():
     print(*i)
