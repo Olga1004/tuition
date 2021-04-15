9d072bc3d48ae0c9141c1222913244a4f970eae8
Ольга Серова <moroz9704@icloud.com>
2021-04-15 09:23:08 +0300

    game исправила

--- week2/3неделя/game2.py
+++ week2/3неделя/game2.py
@@ -1,3 +1,4 @@
+
 class Entity():
     def __init__(self, defense, life): # защита. жизнь
         self.defense = defense
@@ -9,50 +10,62 @@ class Predator(Entity):
         super().__init__(defense, life)
         self.damage = damage
 
-    def attack(self, score):
-        self.life = self.life - score
+    def attack(self, target, coord_x, coord_y):
+        if target.x == coord_x and target.y == coord_y:
+            if self.damage < target.defense:
+                target.defense = target.defense - self.damage
+                print('Вы поранили цель')
+            else:
+                target.defense = 0
+                target.life = target.life - (self.damage - target.defense)
+                print('Вы отняли всю защиту у соперника')
+        else:
+            print('Вы промахнулись')
 
-    def get_life(self):
-        return self.life
+    def location(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_defense(self):
+        return self.defense
 
 
 class Soldier(Predator):
-    def __init__(self, firing_range, damage, defense, life): # дальность стрельбы
+    def __init__(self, firing_range, damage, defense, life):
         super().__init__(damage, defense, life)
         self.firing_range = firing_range
 
-    def attack(self, score):
-        if self.firing_range <=1:
-            self.life = self.life - score
-        else:
-            self.life = self.life - score * self.firing_range / 100
-
 
 class Commander(Soldier):
-    def __init__(self, team, firing_range, damage, defense, life):
+    def __init__(self, firing_range, damage, defense, life, sold):
         super().__init__(firing_range, damage, defense, life)
-        self.team = 5
+        self.team = input('Введите имена солдат через запятую: ').split(',')
+        self.sold = sold
 
-    def attacks(self, score1, score2, score3, score4, score5):
-        self.score1 = score1
-        self.score2 = score2
-        self.score3 = score3
-        self.score4 = score4
-        self.score5 = score5
-        self.life = self.life - (score1+score2+score3+score4+score5+self.damage)
+    def attack(self, target, coord_x, coord_y):
+        if target.x == coord_x and target.y == coord_y:
+            if self.damage + len(self.team) * self.sold.damage < target.defense:
+                target.defense = target.defense - (self.damage + len(self.team) * self.sold.damage)
+                print('Вы поранили цель')
+            else:
+                target.defense = 0
+                target.life = target.life - (self.damage + len(self.team) * self.sold.damage - target.defence)
+                print('Вы отняли всю защиту у соперника')
+        else:
+            print('Вы не попали в соперника')
 
-    def get_life(self):
-        return self.life
 
-
-sol1 = Soldier(0, 1000, 1200, 2000)
-pred1 = Predator(1100, 800, 2000)
-comm = Commander(5, 1, pred1.life, 1500, 2500)
-comm.attacks(1200, 200, 300, 400, 500)
-print(comm.get_life())
-sol1.attack(pred1.damage)
-print(sol1.get_life())
-
+sol1 = Soldier(1, 20, 160, 300)
+sol1.location(10, 20)
+pred1 = Predator(25, 200, 500)
+pred1.location(10, 15)
+sol1.attack(pred1, 10, 15)
+print(pred1.get_defense())
+pred1.attack(sol1, 10, 20)
+print(sol1.get_defense())
+comm1 = Commander(10, 30, 180, 350, sol1)
+comm1.attack(pred1, 10, 15)
+print(pred1.get_defense())
 
 
 
